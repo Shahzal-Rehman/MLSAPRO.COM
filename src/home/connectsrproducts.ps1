9162015f5df98278210f59b7a23acb111052d8f6
Sr555768 <155820504+Sr555768@users.noreply.github.com>
2024-04-28 16:11:40 +0500

    Add files via upload
    Signed-off-by: Sr555768 <155820504+Sr555768@users.noreply.github.com>

---
+++ src/home/connectsrproducts.ps1
@@ -0,0 +1,9 @@
+﻿$connectTestResult = Test-NetConnection -ComputerName msproducts.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    cmd.exe /C "cmdkey /add:`"msproducts.file.core.windows.net`" /user:`"localhost\msproducts`" /pass:`"3AHKeiAWdrSrxwQZLurSUEx6/JQm8PLdbjDUg/WYU9CEuY1zXSj6DS4vIt1ciYTQ0LAxbDOZ3u9V+AStzRQrLQ==`""
+    # Mount the drive
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\msproducts.file.core.windows.net\srproductsbymicrosoft" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
